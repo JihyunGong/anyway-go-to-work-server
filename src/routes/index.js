565efefcb464a27846e1c64861511a613bfcfedb
JihyunGong <jihyun2462@gmail.com>
2022-08-30 01:36:54 +0900

    refactor: Modify api url to be more RESTful

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,6 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post("/", authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
